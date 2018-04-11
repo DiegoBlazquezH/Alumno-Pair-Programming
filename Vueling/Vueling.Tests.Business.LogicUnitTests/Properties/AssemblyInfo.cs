00184c2dc9bd782dfd3d536d2e51c90262bda640
ATMIRA\diego.blazquez <diego.blazquez@atmira.com>
2018-04-11 17:09:49 +0200

    Inicio Nmock

---
+++ Vueling/Vueling.Tests.Business.LogicUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Vueling.Tests.Business.LogicUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Vueling.Tests.Business.LogicUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5a5fa4c8-bf6d-4dd9-862f-781afb97f40a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Vueling/Vueling.Tests.Business.LogicUnitTests/UnitTestsAlumnoBL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NMock;
+using Vueling.Common.Logic.Model;
+
+namespace Vueling.Tests.Business.LogicUnitTests
+{
+    [TestClass]
+    public class UnitTestsAlumnoBL
+    {
+        private readonly MockFactory _factory = new MockFactory();
+        private Mock<ListadoAlumnosJson> SingletonJson;
+        private List<Alumno> ListaAlumnos = new List<Alumno>();
+
+
+        [TestInitialize]
+        public void InitTest()
+        {
+            SingletonJson = _factory.CreateMock<ListadoAlumnosJson>();
+            ListaAlumnos.Add(new Alumno(Guid.NewGuid(), 1, "Diego", "Blázquez", "11111111A", new DateTime(1994, 1, 24), 24, DateTime.Now));
+            ListaAlumnos.Add(new Alumno(Guid.NewGuid(), 2, "Niño", "Pequeño", "22222222B", new DateTime(2000, 12, 3), 17, DateTime.Now));
+            ListaAlumnos.Add(new Alumno(Guid.NewGuid(), 3, "Usuario", "Pruebas", "33333333C", new DateTime(2015, 4, 4), 3, DateTime.Now));
+        }
+
+        [TestCleanup]
+        public void CleanTest()
+        {
+            _factory.ClearExpectations();
+        }
+
+        public static IEnumerable<object[]> DatosAlumno()
+        {
+            yield return new object[] { "", "1", "", "", "", null,  false, "", DateTime.Now, false };
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(DatosAlumno), DynamicDataSourceType.Method)]
+        public void FilterUnitTest(string guid, string nombre, string apellidos, string dni, string id, DateTime dtFechaNacimiento, bool fechaNacimientoChecked, string edad, DateTime dtFechaRegistro, bool fechaRegistroChecked)
+        {
+
+            SingletonJson.Expects
+               .One
+               .Method(s => s.GetList())
+               .WillReturn(ListaTest);
+        }
+    }
+}
